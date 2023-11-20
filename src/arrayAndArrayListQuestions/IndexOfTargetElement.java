d8ec31d4830567730f4867a520fc482c7cce04fb
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2023-11-20 10:37:27 +0100

    issue if element is not in array

--- src/arrayAndArrayListQuestions/IndexOfTargetElement.java
+++ src/arrayAndArrayListQuestions/IndexOfTargetElement.java
@@ -6,20 +6,39 @@ public class IndexOfTargetElement {
 		
 		IndexOfTargetElement i = new IndexOfTargetElement();
 		int[] arr = {1,3,5,6};
-		int value = i.searchInsert(arr, 2);
+		int value = i.searchInsert(arr, 7);
 		
 		System.out.println("index of target " + value);
 	}
 	
 	
 	 public int searchInsert(int[] nums, int target) {
-		 for(int i=0; i<nums.length; i++) {
+		 int l = nums.length;
+		 
+		 for(int i=0; i<l; i++) {
+			 
 			 if(nums[i] == target) {
-				 
 				 target = i;
+				 System.out.println("if " + target);
+				 break;
 			 }
-
-		 }
+			 
+				 if(nums[i] > target) {
+					 target = i-2;
+						System.out.println("else if " + target);
+						
+			 }
+				 else {
+					 for (int j : nums) {
+							if(j != target) {
+								target = nums.length+1;
+								//System.out.println("number not in arrary "  + target);
+							}
+						}
+					 break;
+				 }
+			 }
+		 
 		 
 		return target;
 	        
